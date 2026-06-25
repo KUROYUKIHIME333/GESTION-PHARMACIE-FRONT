a31d182288228f481829b0eeb22c866f25a35f75
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-25 22:36:12 +0100

    UPDATE

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -30,10 +30,20 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
 	}
 
 	return (
-		<div className="min-h-screen bg-background flex">
-			<Sidebar />
-			<div className="flex-1 flex flex-col min-w-0">
-				<Header />
+		// <div className="min-h-screen bg-background flex">
+		// 	<Sidebar />
+		// 	<div className="flex-1 flex flex-col min-w-0">
+		// 		<Header />
+		// 		<main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-auto">
+		// 			<div className="max-w-7xl mx-auto">{children}</div>
+		// 		</main>
+		// 	</div>
+		// </div>
+
+		<div className="flex-1 flex flex-col min-w-0">
+			<Header />
+			<div className="min-h-screen bg-background flex">
+				<Sidebar />
 				<main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-auto">
 					<div className="max-w-7xl mx-auto">{children}</div>
 				</main>
