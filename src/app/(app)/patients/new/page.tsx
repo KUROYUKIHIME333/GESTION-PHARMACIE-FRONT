98b7ec8ba6d686e4b9e32c4b02aaea5d128e9cce
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-30 13:29:43 +0100

    A new patient and a new prescription pages

---
+++ src/app/(app)/patients/new/page.tsx
@@ -0,0 +1,5 @@
+import { PatientForm } from '@/src/components/forms/PatientForm';
+
+export default function NewPatientPage() {
+	return <PatientForm mode="create" />;
+}
---
+++ src/app/(app)/prescriptions/new/page.tsx
@@ -0,0 +1,5 @@
+import { PrescriptionForm } from '@/src/components/forms/PrescriptionForm';
+
+export default function NewPrescriptionPage() {
+	return <PrescriptionForm />;
+}
