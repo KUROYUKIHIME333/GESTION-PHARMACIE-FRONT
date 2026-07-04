095022d318407817a30131f9720e1bd8be2b4aa1
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-07-04 09:34:15 +0100

    make a dashboard store

---
+++ src/stores/dashboard.store.ts
@@ -0,0 +1,55 @@
+import { create } from "zustand";
+import { DashboardStats } from "@/src/types";
+
+interface DashboardState {
+  stats: DashboardStats | null;
+  isLoading: boolean;
+  isFetched: boolean;
+  isError: boolean;
+  lastError: string | null;
+  lastFetched: Date | null;
+}
+
+interface DashboardStore extends DashboardState {
+  setDashboardStats: (stat: DashboardStats | null) => void;
+  setDashboardLoading: (loading: boolean) => void;
+  setDashboardErrors: (isError: boolean) => void;
+  setDashboardIsFetched: (fetched: boolean) => void;
+  setDashboardLastFetched: (lastFetched: Date | null) => void;
+  setDashboardLastError: (lastError: string | null) => void;
+  resetDashboardErrors: () => void;
+  resetDashboardStore: () => void;
+}
+
+const initialDashboardState: DashboardState = {
+  stats: null,
+  isLoading: false,
+  isFetched: false,
+  isError: false,
+  lastError: null,
+  lastFetched: null,
+};
+
+export const useDashboardStore = create<DashboardStore>((set) => ({
+  ...initialDashboardState,
+
+  setDashboardStats: (stats) => set({ stats }),
+
+  setDashboardLoading: (isLoading) => set({ isLoading }),
+
+  setDashboardErrors: (isError) => set({ isError }),
+
+  setDashboardIsFetched: (isFetched) => set({ isFetched }),
+
+  setDashboardLastFetched: (lastFetched) => set({ lastFetched }),
+
+  setDashboardLastError: (lastError) => set({ lastError }),
+
+  resetDashboardErrors: () =>
+    set({
+      isError: false,
+      lastError: null,
+    }),
+
+  resetDashboardStore: () => set(initialDashboardState),
+}));
